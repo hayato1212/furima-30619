59e3746f061b8fde2a873d78663517c1ccc61672
hayato1212 <71763506+hayato1212@users.noreply.github.com>
2020-10-22 19:18:45 +0900

    エラーメッセージを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+  validates :password,        format: { with: VALID_PASSWORD_REGEX , message: "半角英数字混合で記入してください" }
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "ひらがな、カタカナ、漢字のいずれかで記入してください" }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "ひらがな、カタカナ、漢字のいずれかで記入してください" }
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで記入してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで記入してください" }
+  validates :birthday,        presence: true
 end
