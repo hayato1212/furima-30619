3a7faa1b13b4ae281cbd1c1fd9423cb7ead8170c
hayato1212 <71763506+hayato1212@users.noreply.github.com>
2020-10-22 22:04:29 +0900

    ログイン、ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/.freeze
-  validates :password,        format: { with: VALID_PASSWORD_REGEX , message: "半角英数字混合で記入してください" }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
   validates :nickname,        presence: true
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "ひらがな、カタカナ、漢字のいずれかで記入してください" }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "ひらがな、カタカナ、漢字のいずれかで記入してください" }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで記入してください" }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "カタカナで記入してください" }
+  validates :last_name,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,      presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,        presence: true
 end
