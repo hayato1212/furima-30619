e10370dddd67234193efee99a39c4460df73d3ee
hayato1212 <71763506+hayato1212@users.noreply.github.com>
2020-10-25 22:20:43 +0900

    バリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :category_id
+    validates :price
+    validates :status_id
+    validates :description
+    validates :prefecture_id
+    validates :date_of_shipment_id
+    validates :burden_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,5 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
   end
+  has_many :items
 end
