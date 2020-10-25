b6f90bf25a50a75106a3915d5ce77773260088fd
hayato1212 <71763506+hayato1212@users.noreply.github.com>
2020-10-25 22:13:42 +0900

    カラムの追加

--- db/migrate/20201025130159_create_items.rb
+++ db/migrate/20201025130159_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string     :name,                 null:false
+      t.integer    :category_id,          null:false
+      t.integer    :price,                null:false
+      t.integer    :status_id,            null:false
+      t.text       :description,          null:false
+      t.integer    :prefecture_id,        null:false
+      t.integer    :date_of_shiipment_id, null:false
+      t.integer    :burden_id,            null:false
+      t.references :user,                 null:false, forign_key:true
       t.timestamps
     end
   end
