20cfa44fbb70ce29da0718264a65accc3041a1c9
hayato1212 <71763506+hayato1212@users.noreply.github.com>
2020-11-02 16:58:47 +0900

    ログインした人しか削除できないように記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,8 +39,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if current_user.id == @item.user.id
     @item.destroy
     redirect_to root_path
+    else
+    render root_path
+    end
   end
 
   private
